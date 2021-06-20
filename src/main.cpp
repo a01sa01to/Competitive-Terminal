eb3877c46018cc6725bee04baa55668453e851d2
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-06-20 12:13:16 +0900

    Create main.cpp: Add logo(?)

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#define _GLIBCXX_DEBUG
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+  cout << fixed << setprecision(15);
+  cout << endl;
+  cout << "┌──────────────────────────────────────┐" << endl;
+  cout << "│                                      │" << endl;
+  cout << "│   Competitive Programming Terminal   │" << endl;
+  cout << "│                                      │" << endl;
+  cout << "│                          Made by Asa │" << endl;
+  cout << "└──────────────────────────────────────┘" << endl;
+  cout << endl;
+  return 0;
+}
