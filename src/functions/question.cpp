a845df92a486474406f23bf19b8cef3d1e94f321
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-06-21 21:28:42 +0900

    Update question.cpp: 1行だけ入力を読み込む

--- src/functions/question.cpp
+++ src/functions/question.cpp
@@ -7,9 +7,11 @@ string question(string q) {
   cout << "? " << q << endl;
   cout << "> ";
   string ans;
-  cin >> ans;
+  getline(cin, ans);
   cout << endl;
   transform(ans.begin(), ans.end(), ans.begin(),
-            [](unsigned char c) { return tolower(c); });
+            [](unsigned char c) {
+              return tolower(c);
+            });
   return ans;
 }
