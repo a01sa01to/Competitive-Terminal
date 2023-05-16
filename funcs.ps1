757ef530c0847722a8b7104adf9d9743a5b018ec
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-05-16 23:45:04 +0900

    Update funcs.ps1

--- funcs.ps1
+++ funcs.ps1
@@ -1,4 +1,4 @@
-function cp-c() {
+function global:cp-c() {
   Write-Output ">>> Compile a.cpp"
   g++ -o a.exe -std=c++17 -I . -DLOCAL a.cpp
   if ($LASTEXITCODE -ne 0) {
@@ -8,13 +8,13 @@ function cp-c() {
   Write-Output ">>> Compile Success"
 }
 
-function cp-r() {
+function global:cp-r() {
   Write-Output ">>> Reset"
   Copy-Item settings/template.cpp a.cpp
   Write-Output ">>> Reset Success"
 }
 
-function cp-t() {
+function global:cp-t() {
   Write-Output ">>> Test"
   .\a.exe
   if ($LASTEXITCODE -ne 0) {
@@ -24,9 +24,9 @@ function cp-t() {
   Write-Output ">>> Test Success"
 }
 
-function cp-ti() {
+function global:cp-ti() {
   Write-Output ">>> Test (input)"
-  .\a.exe < stdin.txt
+  Get-Content stdin.txt | .\a.exe
   if ($LASTEXITCODE -ne 0) {
     Write-Output ">>> Test Error"
     return
@@ -34,7 +34,7 @@ function cp-ti() {
   Write-Output ">>> Test Success"
 }
 
-function cp-to() {
+function global:cp-to() {
   Write-Output ">>> Test (output)"
   .\a.exe 1> stdout.txt 2> stderr.txt
   if ($LASTEXITCODE -ne 0) {
@@ -44,9 +44,9 @@ function cp-to() {
   Write-Output ">>> Test Success"
 }
 
-function cp-tio() {
+function global:cp-tio() {
   Write-Output ">>> Test (input, output)"
-  .\a.exe < stdin.txt 1> stdout.txt 2> stderr.txt
+  Get-Content stdin.txt | .\a.exe 1> stdout.txt 2> stderr.txt
   if ($LASTEXITCODE -ne 0) {
     Write-Output ">>> Test Error"
     return
